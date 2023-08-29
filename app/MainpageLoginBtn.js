e1dd0d3a01ee2b8324bacaae89eedcafd461a94a
user941211 <common941211@gmail.com>
2023-08-30 03:28:33 +0900

    style 변경

--- app/MainpageLoginBtn.js
+++ app/MainpageLoginBtn.js
@@ -4,7 +4,7 @@ import { signIn } from 'next-auth/react';
 export default function MainpageLoginBtn() {
   return (
     <button 
-    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+    className="m-1 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
     onClick={() => signIn() }>소셜 로그인하기</button>
   )
 }
--- app/MainpageLogoutBtn.js
+++ app/MainpageLogoutBtn.js
@@ -3,7 +3,7 @@ import { signOut } from 'next-auth/react'
 export default function MainpageLoginBtn(){
     return(
         <button 
-        className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+        className="m-1 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
         onClick={() => { alert('로그아웃되었습니다'),signOut() }}>로그아웃</button>
     )
 }
