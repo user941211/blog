9b6e1ca6e68f5f705e8e7ad0951e9c29e1136c3e
user941211 <common941211@gmail.com>
2023-09-04 11:23:51 +0900

    코드 일부분 수정

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -2,17 +2,14 @@ import { ObjectId } from "mongodb";
 import { connectDB } from "/util/database"
 import Link from 'next/link'
 import Deletebtn from "./router_btn";
+import Comment from "./comment"
 
 export default async function Detail(props) {
     let db = (await connectDB).db(`${process.env.DB_DB}`);
     let result = await db.collection(`${process.env.DB_LIST}`).findOne({
         _id: new ObjectId(props.params.id)
     })
-    // let result1 = await db.collection(`${process.env.DB_USER}`).findOne({
-    //     email : (result.author).toString()
-    // })
-    // console.log(result1)
-    // 추후에 업데이트할것
+    
     return (
         <div className="bg-white p-6 w-4/5 mx-auto">
             <table className="w-full border border-collapse border-gray-300">
@@ -29,17 +26,13 @@ export default async function Detail(props) {
                         <td className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider w-[25%]">작성일</td>
                         <td className="px-6 py-3 border border-gray-300 whitespace-nowrap">{result.date}</td>
                     </tr>
-                    {/* <tr className="border-b border-gray-300">
-                        <td className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider w-[25%]">조회수</td>
-                        <td className="px-6 py-3 border border-gray-300 whitespace-nowrap">{result.views}</td>
-                    </tr> */}
                     <tr>
                         <td className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider w-[25%]">내용</td>
                         <td className="px-6 py-3 border border-gray-300 w-[75%]">{result.content}</td>
                     </tr>
                 </tbody>
             </table>
-            
+            <Comment id={ result._id.toString() }/>
             <div className="w-full mx-auto">
                 <Link href={`/edit/${result._id}`}><button className="text-base m-2 p-5 border border-black rounded-xl">수정하기</button></Link>
                 <Deletebtn id={props.params.id}/>
@@ -49,8 +42,4 @@ export default async function Detail(props) {
         </div>
     )
 }
-{/* <td className="px-6 py-4 whitespace-nowrap">{result.author}</td>
-<td className="px-6 py-4 whitespace-nowrap">{result.date}</td>
-<td className="px-6 py-4 whitespace-nowrap">{result.views}</td> 
-db확장후 추후 수정*/}
                         
